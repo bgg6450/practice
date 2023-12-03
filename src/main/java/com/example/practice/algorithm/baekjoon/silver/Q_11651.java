aec70d8f8c1cad5b34955037c28d466c56c37683
baek <bgg6450@gmail.com>
2023-12-03 17:11:58 +0900

    feat.baekjoon:11651

--- src/main/java/com/example/practice/algorithm/baekjoon/silver/Q_11651.java
+++ src/main/java/com/example/practice/algorithm/baekjoon/silver/Q_11651.java
@@ -3,23 +3,32 @@ package com.example.practice.algorithm.baekjoon.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Q_11651 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
+        int[][] arr = new int[N][2];
         for (int i = 0; i < N; i++) {
             String[] s = br.readLine().split(" ");
-            Map<Integer, Integer> m = new HashMap<>();
-            int x = Integer.parseInt(s[0]);
-            int y = Integer.parseInt(s[1]);
-            m.put(x, y);
+            arr[i][0] = Integer.parseInt(s[1]);
+            arr[i][1] = Integer.parseInt(s[0]);
         }
-
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            }
+            else {
+                return o1[1] - o2[1];
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int[] i : arr) {
+            sb.append(i[1]).append(" ").append(i[0]).append("\n");
+        }
+        System.out.println(sb);
     }
 
 }
