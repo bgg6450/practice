0660f7e5778f6ee1aa17ebe2a5ef19142938f266
baek <bgg6450@gmail.com>
2023-12-03 23:43:52 +0900

    feat.baekjoon:1094,1789

---
+++ src/main/java/com/example/practice/algorithm/baekjoon/silver/Q_1094.java
@@ -0,0 +1,25 @@
+package com.example.practice.algorithm.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_1094 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
+        int std = 64;
+        int count = 0;
+        while (true) {
+            if (i >= std) {
+                i -= std;
+                count++;
+            }
+            if (i == 0) {
+                System.out.println(count);
+                break;
+            }
+            std /= 2;
+        }
+    }
+}
---
+++ src/main/java/com/example/practice/algorithm/baekjoon/silver/Q_1789.java
@@ -0,0 +1,26 @@
+package com.example.practice.algorithm.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_1789 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long i = 0;
+        while (n > std(i)) {
+            i++;
+        }
+        if (n == std(i)) {
+            System.out.println(i);
+        } else {
+            System.out.println(i-1);
+        }
+    }
+
+    public static long std(long n) {
+        return n * (n + 1) / 2;
+    }
+}
